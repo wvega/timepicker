dad6015cd28fcc1d304cb940f04d17367c64bd3e
Willington Vega <wvega@wvega.com>
2013-04-19 09:48:57 -0500

    Update page to use latest Bootstrap.

---
+++ resources/js/timepicker.page.js
@@ -0,0 +1,46 @@
+$(function() {
+
+    if (!$('.nav li a[href="' + document.location.hash + '"]').tab('show').length) {
+        $('.nav li a:first').tab('show');
+    }
+
+    $('a[data-toggle="tab"]').on('shown', function(event) {
+        var link = $(event.target);
+        $('.nav li').removeClass('active');
+        $('.nav li a[href="' + link.attr('href') + '"]').closest('li').addClass('active');
+    });
+
+    $('a[data-toggle="tab"]').on('shown', function(event) {
+        var link = $(event.target);
+        $('.nav li').removeClass('active');
+        $('.nav li a[href="' + link.attr('href') + '"]').closest('li').addClass('active');
+
+        $('html, body').animate({
+            scrollTop: $(link.attr('href')).offset().top - 75
+        }, 500);
+    });
+
+    /* activate timepickers */
+
+    $('#options-time-format').timepicker({ timeFormat: 'h:mm:ss p' });
+
+    $('#options-time-constraints').timepicker({
+        timeFormat: 'HH:mm:ss',
+        minTime: '11:45:00',
+        maxHour: 20,
+        maxMinutes: 30,
+        startTime: new Date(0,0,0,15,0,0),
+        interval: 15
+    });
+
+    $('#options-change-event').timepicker({
+        change: function(time) {
+            // the input field
+            var element = $(this), text;
+            // get access to this TimePicker instance
+            var timepicker = element.timepicker();
+            text = 'Selected time is: ' + timepicker.format(time);
+            element.siblings('span.help-line').text(text);
+        }
+    });
+});
