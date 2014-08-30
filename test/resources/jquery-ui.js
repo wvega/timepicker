8f66fdce5fe1f0ed3bd5d8a800bc843f4ad29c8f
Willington Vega <wvega@wvega.com>
2014-08-30 15:20:49 -0500

    Add new jQuery UI versions to test resources.

--- test/resources/jquery-ui.js
+++ test/resources/jquery-ui.js
@@ -8,14 +8,22 @@
 
     for ( ; i < length; i++ ) {
         current = parts[ i ].split( "=" );
-        if ( current[ 0 ] === "jquery-ui" ) {
+        if ( current[ 0 ] === "jquery-ui" && current[ 1 ].length > 0 ) {
             version = current[ 1 ];
             break;
         }
     }
 
-    css = "resources/jquery-ui/jquery-ui-" + version + "/jquery-ui-" + version + ".custom.css";
-    js = "resources/jquery-ui/jquery-ui-" + version + "/jquery-ui-" + version + ".custom.min.js";
+    if ( version.substr( 0, 3 ) == '1.8' ) {
+        css = "resources/jquery-ui/jquery-ui-" + version + "/jquery-ui-" + version + ".custom.css";
+        js = "resources/jquery-ui/jquery-ui-" + version + "/jquery-ui-" + version + ".custom.min.js";
+    } else if ( version.substr( 0, 3 ) == '1.9' || version.substr( 0, 4 ) == '1.10' ) {
+        css = "resources/jquery-ui/jquery-ui-" + version + "/css/ui-lightness/jquery-ui-" + version + ".custom.min.css";
+        js = "resources/jquery-ui/jquery-ui-" + version + "/js/jquery-ui-" + version + ".custom.min.js";
+    } else if ( version.substr( 0, 3 ) == '1.11' ) {
+        css = "resources/jquery-ui/jquery-ui-" + version + "/css/jquery-ui.min.css";
+        js = "resources/jquery-ui/jquery-ui-" + version + "/js/jquery-ui.min.js";
+    }
 
     document.write( "<link rel='stylesheet' href='" + css + "' type='text/css' />" );
     document.write( "<script src='" + js + "'></script>" );
