116004e65b74de49216a4f05a3a5f74f6e3d0500
Willington Vega <wvega@wvega.com>
2017-08-19 22:23:24 -0500

    Trigger a 'change' event after we update the field (#19).

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -272,8 +272,8 @@
                             i.close();
                         }
                     });
-                }).bind('change.timepicker', function() {
-                    if (i.closed()) {
+                }).bind('change.timepicker', function(event, time) {
+                    if (typeof time === 'undefined' && i.closed()) {
                         i.setTime($.fn.timepicker.parseTime(i.element.val()));
                     }
                 });
@@ -583,6 +583,7 @@
                 // custom change event and change callback
                 // TODO: add documentation about this event
                 if (previous !== null || i.selectedTime !== null) {
+                    i.element.trigger('change', [time]);
                     i.element.trigger('time-change', [time]);
                     if ($.isFunction(i.options.change)) {
                         i.options.change.apply(i.element, [time]);
